420df02264b3dd9cab8bac1dbab4f5d478b2bc7c
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2021-11-17 12:11:30 +0530

    tested json and styled todos

--- js/maindummy.js
+++ js/maindummy.js
@@ -1,31 +1,32 @@
-
 window.addEventListener('DOMContentLoaded',()=> renderTasks());
 const tasks_div = document.getElementById('tasks');
 
 const renderTasks = async () => {
 
-    let tasks_url = 'http://localhost:3000/tasks';
-
-    const response = await fetch(tasks_url);
+    const response = await fetch('../data/dbdummy.json');
     const tasks_obj = await response.json();
 
     console.log(tasks_obj);
 
     let task_template = '';
 
-    tasks_obj.forEach(task => {
+    Array.prototype.forEach.call(tasks_obj.tasks, task => {
         task_template += `
             <div class="single-task">
                 <h2>${task.title}</h2>
                 <p>${task.date}</p>
                 <button>Open Task</button>
                 <a href="#"><i class="fas fa-2x fa-trash-alt"></i></a>
-            </div>
-        `
-    });
+            </div>`
+      });
 
 
     tasks_div.innerHTML = task_template;
 }
+
+
+
+
+
 
 
